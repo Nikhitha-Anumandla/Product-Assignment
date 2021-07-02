8fc0f0f1fab46acf407d5a4ccf96ecc3c17a0aa2
Nikhitha-Anumandla <85636076+Nikhitha-Anumandla@users.noreply.github.com>
2021-07-02 16:57:40 +0530

    Add files via upload

---
+++ ProductValidate.js
@@ -0,0 +1,24 @@
+﻿function validate() {
+    var regName = /^[a-zA-Z]+$/;
+    if ($("#pName").val()== "") {
+        alert("Product Name is empty");
+    }
+    if (!regName.test($("#pName").val())) {
+        alert("Product NAme id invalid");
+    }
+    if ($("#price").val() == "") {
+        alert("enter price");
+    }
+    else if (parseInt($("#price").val()) < 1500 || parseInt($("#price").val()) > 3000) {
+        alert("price must in between 1500 and 3000");
+    }
+    if ($("#brand").val() == "") {
+        alert("enter brand");
+    }
+    if ($("#quantity").val() == "") {
+        alert("enter quantity");
+    }
+    if ($("#rememberme").prop('checked') == false) {
+        alert("select check box!");
+    }
+}
